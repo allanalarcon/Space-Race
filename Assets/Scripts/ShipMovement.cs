0a7ec89b4abb0822d44ece27be51e613849e7274
allanalarcon <samuel.alarcon@outlook.com>
2018-07-17 20:56:27 -0500

    First Commit

---
+++ Assets/Scripts/ShipMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ShipMovement : MonoBehaviour {
+
+	Rigidbody rb;
+	public float Velocidad = 8f;
+	public float delay;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 DireccionX = Input.GetAxis ("Horizontal") * Vector3.right;
+		Vector3 DireccionZ = Input.GetAxis ("Vertical") * Vector3.forward;
+
+		Vector3 Direccion = DireccionX + DireccionZ;
+		Vector3 VectorVelocidad = Direccion * Velocidad;
+
+		rb.velocity = VectorVelocidad;
+	}
+}
