6590a9cd4873a8e738163f29153fd0a850e23d5c
DarNtn <dncarpio@fiec.espol.edu.ec>
2018-07-25 00:28:51 -0500

    update

---
+++ Assets/Scripts/BGanimation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGanimation : MonoBehaviour {
+
+	Transform tr;  
+    private float maxScale = 2.45f;
+    private float minScale = 1.2f;
+    private float escala, x, y, z;
+
+	// Use this for initialization
+	void Start () {
+		tr = GetComponent<Transform>();
+        x = y = z = minScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        escala = 0.01f * Time.deltaTime;
+        if (x < maxScale)
+        {
+            x += escala;
+            y += escala;
+            z += escala;
+        }
+
+        tr.localScale = new Vector3(x, y, z);
+	}
+}
