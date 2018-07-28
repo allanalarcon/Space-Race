216ff05565bb2cc0dc26593baccc93f8cc87b629
DarNtn <dncarpio@fiec.espol.edu.ec>
2018-07-27 23:13:36 -0500

    update

--- Assets/Scripts/BGanimation.cs
+++ Assets/Scripts/BGanimation.cs
@@ -5,26 +5,31 @@ using UnityEngine;
 public class BGanimation : MonoBehaviour {
 
 	Transform tr;  
-    private float maxScale = 2.45f;
-    private float minScale = 1.2f;
+    public float maxScale = 2.45f;
+    public float minScale = 1.2f;
     private float escala, x, y, z;
+    public float factorEscala;
+    public float factorRotacion;
 
 	// Use this for initialization
 	void Start () {
 		tr = GetComponent<Transform>();
-        x = y = z = minScale;
+        x = tr.localScale.x;
+        y = tr.localScale.y;
+        z = tr.localScale.z;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        escala = 0.03f * Time.deltaTime;
-        if (x < maxScale)
+        escala = factorEscala * Time.deltaTime;
+        if (x <= maxScale && x >= minScale)
         {
             x += escala;
             y += escala;
             z += escala;
         }
 
-        tr.localScale = new Vector3(x, y, z);
+        tr.localScale = new Vector3(x, y, z);        
+        tr.Rotate(Vector3.forward, factorRotacion);
 	}
 }
