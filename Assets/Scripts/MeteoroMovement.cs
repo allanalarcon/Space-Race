3c47ea3f8feeb832c12597d41dacffc79c76331b
DarNtn <dncarpio@fiec.espol.edu.ec>
2018-07-31 16:22:43 -0500

    update

---
+++ Assets/Scripts/MeteoroMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteoroMovement : MonoBehaviour {
+
+	public float velocidad = 20f;
+	
+	// Use this for initialization
+	void Start () {		
+		Destroy(gameObject, 4);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position -= transform.right * velocidad * Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/MeteoroScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteoroScript : MonoBehaviour {
+
+	public Transform Tmeteoro;
+	public GameObject meteoro;
+	
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Z)){
+			Instantiate(meteoro, Tmeteoro.position, meteoro.transform.rotation);
+		}
+	}
+}
