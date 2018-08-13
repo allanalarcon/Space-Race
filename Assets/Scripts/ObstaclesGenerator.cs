c609cc8188006f7f732ba5d14f405506041de334
DarNtn <dncarpio@fiec.espol.edu.ec>
2018-08-13 01:25:41 -0500

    update 12/08

--- Assets/Scripts/ObstaclesGenerator.cs
+++ Assets/Scripts/ObstaclesGenerator.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ObstaclesGenerator : MonoBehaviour {
-	
+
+    public float instanceRate=2f;
+    public int instanceAmount=3;
 	public GameObject meteoro;    
     public GameObject lluvia;
     public GameObject agujero;
@@ -12,11 +14,7 @@ public class ObstaclesGenerator : MonoBehaviour {
     public GameObject satelite1;
     public GameObject satelite2;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+
 	// Update is called once per frame
 	void Update () {
         
@@ -46,6 +44,19 @@ public class ObstaclesGenerator : MonoBehaviour {
 
         if (Input.GetKeyDown(KeyCode.D)) {
             Instantiate(satelite2, new Vector3(Random.Range(20, 25), Random.Range(-7, 7), 0), satelite2.transform.rotation);
+        }
+    }
+
+    IEnumerator Start() {
+        GameObject[] obstaculos = {meteoro, lluvia, robot, roca, satelite1, satelite2};
+        while (true) {
+
+            for (int i=0; i<instanceAmount; i++) {
+                GameObject ob = obstaculos[Random.Range(0, 5)];
+                Instantiate(ob, new Vector3(Random.Range(20, 25), Random.Range(-7, 7), 0), ob.transform.rotation);
+            }            
+
+            yield return new WaitForSeconds(instanceRate);
         }
     }
 }
